a8ea3ffb3a39cc6b36c4c5224e9f4598a091a2bb
Kaleb VanderSys <kaleb@pixellight.com>
2025-09-01 18:59:41 -0600

    Additional tests added

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-import {helloWorld, add} from '../js/main.js';
+import {helloWorld, add, fetchRandomJoke} from '../js/main.js';
 // Import the sinon library to allow us to create a spy on the console.log function
 import sinon from 'sinon';
 
@@ -45,6 +45,27 @@ QUnit.module('main.js tests', function() {
         const result = add(num1, num2);
         //Assert
         assert.equal(result, expected, 'add(2, -3) should return -1');
+    });
+
+    QUnit.test('add should return the sum of a positive and zero', function(assert) {
+        //Arrange
+        const num1 = 2;
+        const num2 = 0;
+        const expected = 2;
+        //Act
+        const result = add(num1, num2);
+        //Assert
+        assert.equal(result, expected, 'add(2, -3) should return -1');
+    });
+
+    QUnit.test('fetchRandomJoke should return a string', function(assert) {
+        //Arrange
+        let testJoke = "test123";
+        //Act
+        testJoke = fetchRandomJoke();
+        //Assert
+        assert.true(testJoke != null, 'fetchRandomJoke should not return null');
+        assert.false(testJoke == "test123", 'fetchRandomJoke should reassign the value of testJoke');
     });
 
 });
